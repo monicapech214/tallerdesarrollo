97a0bca8cf5b59ab195a5b93856d5a27702b2b3c
Yamilecen12 <centrinidad74@gmail.com>
2022-06-22 21:08:24 -0500

    agregado

---
+++ app/src/main/java/com/example/primerparcialnoveno/formulario_registro.kt
@@ -0,0 +1,11 @@
+package com.example.primerparcialnoveno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class formulario_registro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_registro)
+    }
+}
